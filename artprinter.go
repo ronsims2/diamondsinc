26b5dd7e6cacff29cece4c92efec179359d2812a
Ronald Sims II <ron@orangemantis.net>
2019-03-23 12:29:50 -0400

    Fixed time by installing go routine and added print delim.

--- artprinter.go
+++ artprinter.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 )
 
-//Print the content of a file
+//PrintArt prints the content of a file
 func PrintArt(artName string) {
 	artFile, _ := os.Open(artName + ".art")
 	scanner := bufio.NewScanner(artFile)
@@ -15,4 +16,15 @@ func PrintArt(artName string) {
 		//fmt.Printf(scanner.Text())
 		fmt.Println(scanner.Text())
 	}
+}
+
+//PrintDelim prints a line of the same character to break up sections
+func PrintDelim(delimChar string, length int) {
+	var delim bytes.Buffer
+
+	for i := 0; i < length; i++ {
+		delim.WriteString(delimChar)
+	}
+
+	fmt.Println(delim.String())
 }
--- diamondincs.go
+++ diamondincs.go
@@ -20,9 +20,11 @@ func main() {
 	playerName, _ := reader.ReadString('\n')
 	playerName = strings.TrimSpace(playerName)
 	fmt.Println("Well then, welcome " + playerName + " I am sure you will do fine here.")
-	time.Sleep(2 * time.Second)
+	time.Sleep(3.5 * time.Second)
+	PrintDelim("=", 80)
 	fmt.Println("As the store manager it's your job to make sure that our customers are happy. \nIt is important to make sure that you stock the right merch and properly engage the consumers.")
-	time.Sleep(2 * time.Second)
+	time.Sleep(3.5 * time.Second)
+	PrintDelim("=", 80)
 	fmt.Println("What would you like to do first? I recommend that you check the books.")
 
 	playRound()
